ed01716b99ff85699d56fd39f7112565688b14c0
Yunshu <yunshudai2010@gmail.com>
2022-08-21 15:03:39 +1000

    Initial Commit

---
+++ Cuppa/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cuppa
+//
+//  Created by Yunshu D on 21/8/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
